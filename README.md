f4a4b8e326c5f33a64d33aa5e8eb226dc3150002
Maurelian <maurelian@protonmail.ch>
2022-02-17 18:38:58 -0700

    Add console.sol, add numbers to files

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 Just a simple forge repo for playing around with solidity toy code.
 
+## Conventions
+
+The whole purpose of this repo is to make it fast and easy to test stuff, and then keep the test code for
+future reference. Specific test contracts can be called with
+
+`forge test --match-contract Test3`
+
+I don't want to have to worry about avoiding contract naming collision, so each new test file
+is prefixed with a number, and all the contract names in that file have that same number as a suffix.
+
 
---
+++ src/test/3_StructArgs.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+import "ds-test/test.sol";
+import './console.sol';
+
+contract Data3 {
+  struct Args {
+    bytes14 padding;
+    bytes16 shortenedAmount;
+    bytes2 assetId;
+  }
+}
+
+contract Target3 is Data3 {
+
+  function submitBytes32Compressed(bytes32 args) external {
+    console.log(msg.data.length); // 36
+    console.logBytes(msg.data);
+  }
+
+  // can you compress data by using a struct for an arg?
+  function submitStrArgs(Args calldata args) external {
+    console.log(msg.data.length); // 100
+    console.logBytes(msg.data);
+  }
+}
+
+contract Test3 is Data3 {
+    Target3 t;
+    bytes32 foo = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+
+    function setUp() public {
+        t = new Target3();
+    }
+
+    function test_1() external {
+      t.submitBytes32Compressed(foo);
+    }
+
+    function test_2() external {
+      Args memory a = Args({
+        padding: 0xffffffffffffffffffffffffffff,
+        shortenedAmount: 0xffffffffffffffffffffffffffffffff,
+        assetId: 0xffff
+      });
+      t.submitStrArgs(a);
+    }
+}
+
+
+
+
