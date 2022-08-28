d6a80c0ed0edbf36b051826539b747dda61e7908
Maurelian <maurelian@protonmail.ch>
2022-08-27 20:10:16 -0400

    test: Malleability
    --wip--

--- test/21_malleability.sol
+++ test/21_malleability.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
-import "forge-std/Test.sol";
+// pragma solidity 0.8.15;
+// import "forge-std/Test.sol";
 
-contract Test21 is Test {
+// contract Test21 is Test {
 
-    function test_malleability() external {
+//     function test_malleability() external {
 
-    }
+//     }
 
-    function giveMoney() exernal {
+//     function giveMoney() exernal {
 
-        require(whitelist[msg.sender);
-        address addr = ECDSA.recover(msg.data);
+//         require(whitelist[msg.sender);
+//         address addr = ECDSA.recover(msg.data);
 
-    }
-}
+//     }
+// }
