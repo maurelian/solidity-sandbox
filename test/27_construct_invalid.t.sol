3ad69cc45a8256996d3f62f1d0259e276af017cd
Maurelian <maurelian@protonmail.ch>
2023-05-19 13:37:03 -0400

    27

--- test/27_construct_invalid.t.sol
+++ test/27_construct_invalid.t.sol
@@ -9,7 +9,7 @@ contract Test27 is Test {
       mstore(0x40, 0x6000)
       mstore(0x6000, 0xfe)
       // call the constructor
-      pop(create(0, 0x6000, 1, 0, 0))
+      pop(create(0, 0x6000, 1))
     }
   }
 }
